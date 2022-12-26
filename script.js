48197f79da0e11d39f5d591067bbe8a14bdf8b27
harindu-pe <79392870+harindu-pe@users.noreply.github.com>
2022-12-26 14:58:56 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+const mainButton = document.getElementById("main-button");
+const mainImage = document.getElementById("main-image");
+const mainContainer = document.getElementById("container");
+const display = document.getElementById("display");
+const buttonLeft = document.getElementById("button-left");
+const buttonRight = document.getElementById("button-right");
+
+mainButton.addEventListener("click", showGame);
+
+function showGame() {
+  mainButton.classList.toggle("active");
+  mainImage.classList.toggle("active");
+  mainContainer.classList.toggle("active");
+}
+
+buttonRight.addEventListener("click", next);
+buttonLeft.addEventListener("click", prev);
+
+let i = 0; // current image
+let j = 4; // total images
+
+function next() {
+  document.getElementById("content" + (i + 1)).classList.remove("active");
+  i = (j + i + 1) % j;
+  document.getElementById("content" + (i + 1)).classList.add("active");
+}
+
+function prev() {
+  document.getElementById("content" + (i + 1)).classList.remove("active");
+  i = (j + i - 1) % j;
+  document.getElementById("content" + (i + 1)).classList.add("active");
+}
